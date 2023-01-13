5350b1359eed14d0229f64a2f2dc965590db285b
MateusCassio10 <mateuscassio07@gmail.com>
2023-01-13 12:11:15 -0300

    Iniciando o projeto

---
+++ src/main/java/com/Attornatusmateustest/AttornatusMateusTestApplication.java
@@ -0,0 +1,13 @@
+package com.Attornatusmateustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusMateusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusMateusTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Attornatusmateustest/AttornatusMateusTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Attornatusmateustest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusMateusTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
