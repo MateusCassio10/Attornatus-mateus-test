064ea2931ba8bf50813ceb5a6e98a5fee512bae9
MateusCassio10 <mateuscassio07@gmail.com>
2023-01-17 11:06:24 -0300

    Formatando a data do JSON

--- src/main/java/com/Attornatusmateustest/model/Person.java
+++ src/main/java/com/Attornatusmateustest/model/Person.java
@@ -1,9 +1,12 @@
 package com.Attornatusmateustest.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +17,7 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private Date birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
